cb21ab7798864cbf8fb23633df188e6de6dbea3f
Balivadasaikrishna <112052449+Balivadasaikrishna@users.noreply.github.com>
2022-08-26 09:30:58 +0530

    newfileuploaded

---
+++ NormalCode.java
@@ -0,0 +1,42 @@
+package mavendemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+
+public class NormalCode {
+
+	
+	@Parameters({"username", "password"})
+	
+	@Test
+	
+	
+	
+	
+	public void run (String pass , String user) {
+		
+		
+		WebDriver driver;
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\baliv\\Desktop\\chromedriver.exe");
+		 driver = new ChromeDriver();
+		 
+		 driver.get("https://www.facebook.com/login/");
+		 
+		 driver.findElement(By.xpath("//input[@id=\"email\"]")).sendKeys(pass);
+		 driver.findElement(By.xpath("//input[@id=\"pass\"]")).sendKeys(user);
+		 
+		 
+		
+		
+		
+
+	}
+	
+	
+
+}
